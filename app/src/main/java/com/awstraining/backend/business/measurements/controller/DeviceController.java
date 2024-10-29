5fea74e5bf4960a01b70cd2bc1353371abe435d0
Dominika Czycz <external.dominika.czycz@kuehne-nagel.com>
2024-10-29 11:16:22 +0100

    adding counters

--- app/src/main/java/com/awstraining/backend/business/measurements/controller/DeviceController.java
+++ app/src/main/java/com/awstraining/backend/business/measurements/controller/DeviceController.java
@@ -9,6 +9,8 @@ import com.awstraining.backend.api.rest.v1.model.Measurement;
 import com.awstraining.backend.api.rest.v1.model.Measurements;
 import com.awstraining.backend.business.measurements.MeasurementDO;
 import com.awstraining.backend.business.measurements.MeasurementService;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,21 +24,27 @@ class DeviceController implements DeviceIdApi {
     private static final Logger LOGGER = LogManager.getLogger(DeviceController.class);
 
     private final MeasurementService service;
+    private final MeterRegistry registry;
 
     @Autowired
-    public DeviceController(final MeasurementService service) {
+    public DeviceController(final MeasurementService service, MeterRegistry registry) {
         this.service = service;
+        this.registry = registry;
     }
 
     @Override
     public ResponseEntity<Measurement> publishMeasurements(final String deviceId, final Measurement measurement) {
+        Counter counter = registry.counter("publishMeasurements.counter", "key: method value: publishMeasurements");
+
         LOGGER.info("Publishing measurement for device '{}'", deviceId);
         final MeasurementDO measurementDO = fromMeasurement(deviceId, measurement);
         service.saveMeasurement(measurementDO);
+        counter.increment();
         return ResponseEntity.ok(measurement);
     }
     @Override
     public ResponseEntity<Measurements> retrieveMeasurements(final String deviceId) {
+        Counter counter = registry.counter("retrieveMeasurements.counter", "key: method value: retrieveMeasurements");
         LOGGER.info("Retrieving all measurements for device '{}'", deviceId);
         final List<Measurement> measurements = service.getMeasurements()
                 .stream()
@@ -45,6 +53,7 @@ class DeviceController implements DeviceIdApi {
         final Measurements measurementsResult = new Measurements();
         measurementsResult.measurements(measurements);
         LOGGER.info("Measurements size '{}'", measurements.size());
+        counter.increment();
         return ResponseEntity.ok(measurementsResult);
     }
 
